c00b24f8b0ffffea18a094ec6f7a8199fff9045a
Lietuvis hakeris <vaclovkacinskas@gmail.com>
2024-05-05 20:02:59 +0300

    fiч

--- catalog/item.js
+++ catalog/item.js
@@ -1,54 +1,63 @@
 let items = [];
-    function loadXML(url) {
-        return fetch(url)
-            .then(response => response.text())
-            .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-            .then(data => {
-                items = Array.from(data.querySelectorAll('item')).map(item => {
-                    return {
-                        name: item.querySelector('name').textContent,
-                        img: item.querySelector('img').textContent,
-                        about: item.querySelector('about').textContent,
-                        type: item.querySelector('type').textContent,
-                        maker: item.querySelector('maker').textContent,
-                        speed: parseInt(item.querySelector('speed').textContent, 10)
-                    };
-                });
-                return items;
+function loadXML(url) {
+    return fetch(url)
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+            items = Array.from(data.querySelectorAll('item')).map(item => {
+                return {
+                    name: item.querySelector('name').textContent,
+                    img: item.querySelector('img').textContent,
+                    about: item.querySelector('about').textContent,
+                    type: item.querySelector('type').textContent,
+                    maker: item.querySelector('maker').textContent,
+                    speed: parseInt(item.querySelector('speed').textContent, 10)
+                };
             });
-    }
-
-    function addItemsToDOM(items, nameURL) {
-        const itemDetailsContainer = document.querySelector('.info'); // Выберите контейнер для деталей товара
-        items.forEach(item => {
-            if (nameURL === item.name){
-                console.log('Товар найден!');
-                // Обновление содержимого контейнера деталей товара
-                itemDetailsContainer.querySelector('.name').textContent = item.name;
-                itemDetailsContainer.querySelector('.maker').textContent = item.maker;
-                itemDetailsContainer.querySelector('.type').textContent = item.type;
-                itemDetailsContainer.querySelector('.about').textContent = item.about;
-                itemDetailsContainer.querySelector('.image').src = item.img;
-                itemDetailsContainer.querySelector('.mail').href = `mailto:info@printinvest.by?subject=Покупка ${item.name}`;
-                document.title = item.name;
-            }
+            return items;
         });
-    }
+}
+
+function addItemsToDOM(items, nameURL) {
+    const itemDetailsContainer = document.querySelector('.info'); // Выберите контейнер для деталей товара
+    items.forEach(item => {
+        if (nameURL === item.name) {
+            console.log('Товар найден!');
+            // Обновление содержимого контейнера деталей товара
+            itemDetailsContainer.querySelector('.name').textContent = item.name;
+            itemDetailsContainer.querySelector('.maker').textContent = item.maker;
+            itemDetailsContainer.querySelector('.type').textContent = item.type;
+            itemDetailsContainer.querySelector('.about').textContent = item.about;
+            itemDetailsContainer.querySelector('.image').src = item.img;
+            itemDetailsContainer.querySelector('.mail').href = `mailto:info@printinvest.by?subject=Покупка ${item.name}`;
+            document.title = item.name;
+            var link = document.createElement('link');
+
+            // Установка атрибутов для элемента <link>
+            link.rel = 'icon';
+            link.type = 'image/x-icon';
+            link.href = item.img; // Путь к вашей иконке
+
+            // Добавление элемента <link> в <head> документа
+            document.head.appendChild(link);
+        }
+    });
+}
 
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const params = new URLSearchParams(window.location.search);
-        const nameURL = decodeURIComponent(params.get('name'));
-        loadXML('/Printinvest/catalog/items/items.xml')
+document.addEventListener('DOMContentLoaded', function () {
+    const params = new URLSearchParams(window.location.search);
+    const nameURL = decodeURIComponent(params.get('name'));
+    loadXML('/Printinvest/catalog/items/items.xml')
         .then(loadedItems => addItemsToDOM(loadedItems, nameURL)) // Исправленный вызов функции
         .catch(error => console.error('Ошибка при загрузке XML:', error));
 
-        document.querySelectorAll('.mail').forEach(mailLink => {
-            mailLink.addEventListener('click', function(event) {
-                event.preventDefault();
-                window.location.href = "/Printinvest/form.html?name="+encodeURIComponent(nameURL);
-            });
+    document.querySelectorAll('.mail').forEach(mailLink => {
+        mailLink.addEventListener('click', function (event) {
+            event.preventDefault();
+            window.location.href = "/Printinvest/form.html?name=" + encodeURIComponent(nameURL);
         });
     });
+});
 
-    
+
